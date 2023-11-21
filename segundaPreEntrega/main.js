201c31e7982272cc94a423e4a8e70b8672a16405
Fernando Dominguez <fernando.od@gmail.com>
2023-11-21 01:42:48 -0300

    segundaPreEntrega

--- segundaPreEntrega/main.js
+++ segundaPreEntrega/main.js
@@ -5,6 +5,7 @@
 /* 
 
 Agenda
+menú
 clientes  (nombre, apellido, celular, email)
 ver lista de clientes
 buscar de clientes por nombre y si no esta agendado lo agrego
@@ -25,17 +26,15 @@ let cliente2 = new Clientes("Cecilia", "Suarez", 1157645950, "cecilia@suarez.com
 let cliente3 = new Clientes("Leonardo", "Garcia", 1148769706, "leonardo@garcia.com")
 let cliente4 = new Clientes("Cecilia", "Parra", 1153628564, "cecilia@parra.com")
 
-
-
 let agenda = [cliente1, cliente2, cliente3, cliente4]
 
 function menuAgenda() {
-    let opcionMenu = parseInt(prompt('¿Qué decea hacer?\n1-Ver Clientes | 2-Buscar Cliente | 3-Agendar Cliente  || 4-SALIR'))
+    let opcionMenu = parseInt(prompt('AGENDA TELEFÓNICA V1.0 | Elija una opción\n1-Ver Clientes | 2-Buscar Cliente | 3-Agendar Cliente  || 4-SALIR'))
     if (isNaN(opcionMenu)) {
         if (confirm('Ingresó una opcion incorrecta, desea volver a intentar') === false) {
             return alert('¡Gracias por usar la Agenda Telefónica V1.0!')
         } else {
-            menuAgenda
+            menuAgenda()
         }
     } else if (opcionMenu === 1) {
         verClientes()
@@ -46,49 +45,46 @@ function menuAgenda() {
     } else if (opcionMenu === 4) {
         return alert('¡Gracias por usar la Agenda Telefónica V1.0!')
     }
-
 }
-menuAgenda()
+
+menuAgenda()  // INICIANDO APP
+
+
 function verClientes() {
     console.table(agenda)
+    alert('La lista de clientes se vera por consola')
     menuAgenda()
 }
 
 function buscarCliente() {
-    let palabraClave = prompt("ingresa el nombre del cliente que buscas").toUpperCase().trim()
+    let palabraClave = prompt("Ingresa el nombre del cliente que buscas").toUpperCase().trim()
     let busqueda = agenda.filter((x) => x.nombre.toUpperCase().includes(palabraClave))
 
-
-    if (busqueda != "") {
+    if (busqueda.length > 0) {
         console.table(busqueda)
+        alert('El resultado de la busqueda se verá por consola')
+        menuAgenda()
     } else {
-        alert("No existe ningun cliente que se llame " + palabraClave)
-        let busqueda = confirm("¿Lo queres agendar?")
-
+        let busqueda = confirm("No existe ningun cliente que se llame " + palabraClave + "\n¿Lo queres agendar?")
         if (busqueda == true) {
-            agendarCliente()
+            agendarCliente(palabraClave)
         }
     }
 }
 
-function agendarCliente() {
-
-    let nombre = prompt("ingresa el nombre del cliente").trim()
+function agendarCliente(x) {
+    let nombre = prompt("ingresa el nombre del cliente", x).trim()
     let apellido = prompt("ingresa el apellido del cliente").trim()
     let celular = parseInt(prompt("ingresa el celular del cliente"))
     let email = prompt("ingresa el email del cliente").trim()
-    if (nombre === "" || apellido === "" || isNaN(celular) || email === "") {
-        alert("por favor ingresa valores validos")
-        return
+    if (nombre === "" || apellido === "" || isNaN(celular) || email === "") {      
+        alert("Por favor ingresa valores válidos")
+        console.log(email.indexOf("@"))
+        agendarCliente()
     }
-
     let cliente = new Clientes(nombre, apellido, celular, email)
-
     agenda.push(cliente)
-    
     console.table(agenda)
-    alert('¡el cliente ' + cliente.nombre + ' ha sido agregado con exito!')
+    alert('¡El cliente ' + cliente.nombre + ' ha sido agregado con exito!\nPodrá verlo por consola')
     menuAgenda()
-
-
 }
